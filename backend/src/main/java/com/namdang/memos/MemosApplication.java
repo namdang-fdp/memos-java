5553aa1dc37e269b214d69a3b77441b0c86456af
namdang-fdp <namdangcoder@gmail.com>
2025-10-29 08:13:31 +0700

    init

---
+++ backend/src/main/java/com/namdang/memos/MemosApplication.java
@@ -0,0 +1,13 @@
+package com.namdang.memos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/namdang/memos/MemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namdang.memos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
