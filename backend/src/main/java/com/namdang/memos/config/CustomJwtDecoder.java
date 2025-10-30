26a8604777187d73b28c0cecc1441254725e6c17
namdang-fdp <namdangcoder@gmail.com>
2025-10-30 10:46:41 +0700

    feat: jwt decoder helper

---
+++ backend/src/main/java/com/namdang/memos/config/CustomJwtDecoder.java
@@ -0,0 +1,71 @@
+package com.namdang.memos.config;
+
+import com.namdang.memos.repository.InvalidatedTokenRepository;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Component
+public class CustomJwtDecoder implements JwtDecoder {
+    final InvalidatedTokenRepository invalidatedTokenRepository;
+    NimbusJwtDecoder nimbusJwtDecoder = null;
+
+    @Value("${JWT_SIGNER_KEY_BASE64}")
+    String signerKey;
+
+    @Override
+    public Jwt decode(String token) throws JwtException {
+        try {
+            validateTokenDirectly(token);
+        } catch (JOSEException | ParseException e) {
+            throw new JwtException(e.getMessage());
+        }
+        if (Objects.isNull(nimbusJwtDecoder)) {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+            nimbusJwtDecoder = NimbusJwtDecoder
+                    .withSecretKey(secretKeySpec)
+                    .macAlgorithm(MacAlgorithm.HS512)
+                    .build();
+        }
+        return nimbusJwtDecoder.decode(token);
+    }
+
+    private void validateTokenDirectly(String token) throws JOSEException, ParseException, JOSEException {
+        JWSVerifier verifier = new MACVerifier(signerKey.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        String jwtId = signedJWT.getJWTClaimsSet().getJWTID();
+        if (invalidatedTokenRepository.existsById(jwtId)) {
+            throw new JwtException("Token has been invalidated");
+        }
+
+        boolean isSignatureValid = signedJWT.verify(verifier);
+        Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+        boolean isNotExpired = expirationTime.after(new Date());
+
+        if (!isSignatureValid || !isNotExpired) {
+            throw new JwtException("Token is invalid or expired");
+        }
+    }
+
+}
