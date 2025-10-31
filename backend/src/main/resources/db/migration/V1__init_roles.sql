e5553dbd4bbfd93bcc383a9692d92dd85e20511b
Đặng Phương Nam <namdangcoder@gmail.com>
2025-10-31 23:44:07 +0700

    Revert "Feature/authentication (#2)"
    This reverts commit 4cc6d4c4c595cac513a771e51c12fa0df8e5cc82.

--- backend/src/main/resources/db/migration/V1__init_roles.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-INSERT INTO public.role (id, name, description)
-VALUES
-    (gen_random_uuid(), 'ADMIN',   'Memos Admin'),
-    (gen_random_uuid(), 'MEMBER',  'Memos member')
-ON CONFLICT (name) DO NOTHING;
-
