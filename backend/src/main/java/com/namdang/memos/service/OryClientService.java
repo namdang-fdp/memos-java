c6b48262ef6290a9125ea4c94527c08755febcf2
namdang-fdp <namdangcoder@gmail.com>
2025-11-18 17:04:29 +0700

    feat: add ory reponse dto

--- backend/src/main/java/com/namdang/memos/service/OryClientService.java
+++ backend/src/main/java/com/namdang/memos/service/OryClientService.java
@@ -1,7 +1,8 @@
 package com.namdang.memos.service;
 
+import com.namdang.memos.dto.responses.ory.OryResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface OryClientService {
-    ResponseEntity<String> callWhoAmI(String cookie);
+    ResponseEntity<OryResponse> callWhoAmI(String cookie);
 }
