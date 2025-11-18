80636145ba344163daed77fcb65e114e24fc8651
namdang-fdp <namdangcoder@gmail.com>
2025-11-18 09:41:21 +0700

    feat: better validation and type-safe in login facebook

--- frontend/src/lib/service/auth/type.ts
+++ frontend/src/lib/service/auth/type.ts
@@ -1,9 +1,25 @@
-import z from "zod";
+import z from 'zod';
+
+import type { UiNode } from '@ory/client';
+
+export type NodeType = UiNode['attributes']['node_type'];
+
+export type AttrByType<TType extends NodeType> = Extract<
+    UiNode['attributes'],
+    { node_type: TType }
+>;
+
+export type FacebookData = {
+    action: string;
+    method: string;
+    providerValue: string;
+    csrfToken: string;
+};
 
 export type LoginPayload = {
     email: string;
     password: string;
-}
+};
 
 export type LoginResponse = {
     token: string;
@@ -11,14 +27,16 @@ export type LoginResponse = {
         id: string;
         email: string;
         name: string;
-    }
-}
-
+    };
+};
 
 // ------------- Schema ------------------------
 export const loginSchema = z.object({
-    email: z.string().min(1, "This field cannot be null").email("Invalid email"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
+    email: z
+        .string()
+        .min(1, 'This field cannot be null')
+        .email('Invalid email'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
