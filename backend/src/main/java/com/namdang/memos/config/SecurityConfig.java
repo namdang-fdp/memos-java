33d44edd13c355ff69eb4efe4f7ce42464ab429d
namdang-fdp <namdangcoder@gmail.com>
2025-11-19 21:08:57 +0700

    feat: allow /oidc/ory in white list

--- backend/src/main/java/com/namdang/memos/config/SecurityConfig.java
+++ backend/src/main/java/com/namdang/memos/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.namdang.memos.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
 
 import java.util.Arrays;
 
@@ -32,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         // whitelist endpoint (no authenticate need)
-        String[] baseEndpoints = {"/auth/login", "/auth/register", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"};
+        String[] baseEndpoints = {"/auth/oidc/ory","/auth/login", "/auth/register", "/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"};
         String[] publicEndpoints = Arrays.stream(baseEndpoints).map(endpoint -> endpoint).toArray(String[]::new);
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
