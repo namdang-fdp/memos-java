200c39b4e42b9de0c186c38df91bdebe9ec8b3dc
namdang-fdp <namdangcoder@gmail.com>
2025-11-25 10:42:13 +0700

    feat: add project repo

--- backend/src/main/java/com/namdang/memos/repository/ProjectRepository.java
+++ backend/src/main/java/com/namdang/memos/repository/ProjectRepository.java
@@ -3,8 +3,11 @@ package com.namdang.memos.repository;
 import com.namdang.memos.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
     boolean existsByProjectKey(String projectKey);
+
+    List<Project> findByArchivedFalse();
 }
