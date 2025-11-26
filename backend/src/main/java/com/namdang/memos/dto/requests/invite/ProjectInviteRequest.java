50da500333ae408df6d1cbf5854899bbaae55e50
namdang-fdp <namdangcoder@gmail.com>
2025-11-26 13:21:22 +0700

    chore: just send target email for invite api

--- backend/src/main/java/com/namdang/memos/dto/requests/invite/ProjectInviteRequest.java
+++ backend/src/main/java/com/namdang/memos/dto/requests/invite/ProjectInviteRequest.java
@@ -4,12 +4,13 @@ import com.namdang.memos.enumType.ProjectRole;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProjectInviteRequest {
-    String email;
-    ProjectRole role;
+    String targetUserEmail;
 }
