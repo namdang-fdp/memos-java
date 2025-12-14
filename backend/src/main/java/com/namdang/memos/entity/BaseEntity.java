a3c86d150da54f33a1d08e09fe9cf4ff04a07866
namdang-fdp <namdangcoder@gmail.com>
2025-12-14 23:09:43 +0700

    chore: refactor

--- backend/src/main/java/com/namdang/memos/entity/BaseEntity.java
+++ backend/src/main/java/com/namdang/memos/entity/BaseEntity.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
@@ -17,11 +20,16 @@ import java.util.UUID;
 @Setter
 @ToString
 public class BaseEntity {
-    @Id
-    @GeneratedValue
-    private UUID id;
+    @Id @GeneratedValue private UUID id;
 
     @Column(name = "created_at", updatable = false, nullable = false)
     @CreationTimestamp
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    @Column(name = "is_deleted")
+    private boolean deleted = false;
 }
